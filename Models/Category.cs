54d7504e266fbd4d00e3a67a3ca1de90dfcc9c85
Mahmoud-Elkfrawy <MahmoudElkfrawydev@gmail.com>
2025-09-19 22:39:21 +0300

    Adding Category Model

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GomlaMarket.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int DisplayOrder { get; set; }
+    }
+}
