5d64e392880111e4d8698253650b7fb7933caaa6
Mahmoud-Elkfrawy <MahmoudElkfrawydev@gmail.com>
2025-09-20 13:49:08 +0300

    Adding Table in Category Index page and print every thing in Category DB

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GomlaMarket.Data;
+using GomlaMarket.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GomlaMarket.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _dbContext;
+        public CategoryController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
